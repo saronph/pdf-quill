44765b7e84902e2c5d952e6bbc4f1f72b4cc47a7
Saron Philippi <saron.ph@gmail.com>
2023-11-13 20:13:44 -0300

    added pdf chat styles

---
+++ src/components/chat/ChatContext.tsx
@@ -0,0 +1,69 @@
+import { useMutation } from "@tanstack/react-query";
+import { ReactNode, createContext, useState } from "react";
+import { useToast } from "../ui/use-toast";
+
+type StreamResponse = {
+  addMessage: () => void;
+  message: string;
+  handleInputChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  isLoading: boolean;
+};
+
+export const ChatContext = createContext<StreamResponse>({
+  addMessage: () => {},
+  message: "",
+  handleInputChange: () => {},
+  isLoading: false,
+});
+
+interface ChatContextProviderProps {
+  fileId: string;
+  children: ReactNode;
+}
+
+export const ChatContextProvider = ({
+  fileId,
+  children,
+}: ChatContextProviderProps) => {
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { toast } = useToast();
+
+  const { mutate: sendMessage } = useMutation({
+    mutationFn: async ({ message }: { message: string }) => {
+      const response = await fetch("/api/message", {
+        method: "POST",
+        body: JSON.stringify({
+          fileId,
+          message,
+        }),
+      });
+
+      if (!response.ok) {
+        throw Error("Failed to send message");
+      }
+
+      return response.body;
+    },
+  });
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setMessage(e.target.value);
+  };
+
+  const addMessage = () => sendMessage({ message });
+
+  return (
+    <ChatContext.Provider
+      value={{
+        addMessage,
+        handleInputChange,
+        isLoading,
+        message,
+      }}
+    >
+      {children}
+    </ChatContext.Provider>
+  );
+};
--- src/components/chat/ChatInput.tsx
+++ src/components/chat/ChatInput.tsx
@@ -1,11 +1,36 @@
+import { Send } from "lucide-react";
+import { Button } from "../ui/button";
+import { Textarea } from "../ui/textarea";
+
 interface ChatInputProps {
   isDisabled?: boolean;
 }
 
 export default function ChatInput({ isDisabled }: ChatInputProps) {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="absolute bottom-0 left-0 w-full">
+      <form className="mx-2 flex flex-row gap-3 md:max-4 md:last:mb-6 lg:mx-auto lg:max-w-2xl xl:max-w-3xl">
+        <div className="relative flex h-full flex-1 items-stretch md:flex-col">
+          <div className="relative flex flex-col w-full flex-grow p-4">
+            <div className="relative">
+              <Textarea
+                rows={1}
+                maxRows={4}
+                autoFocus
+                placeholder="Enter your question..."
+                className="resize-none pr-12 text-base py-3 scrollbar-thumb-blue scroll-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch"
+              />
+
+              <Button
+                aria-label="send message"
+                className="absolute bottom-1.5 right-[8px]"
+              >
+                <Send className="h-4 w-4" />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
 }
